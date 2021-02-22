809b787e059943d5656af6ca477e4aceb92c8a57
duranbe <benoit.durand.mail@gmail.com>
2021-02-22 14:12:27 +0100

    update front

--- CoinAlert/front/views.py
+++ CoinAlert/front/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
 	return render(request=request,
 		template_name='home.html')
+def login(request):
+	return render(request=request,
+		template_name='login.html')
